216d084e1489308989b9a21313963bad8b56fe90
pwony <p.wony0526@gmail.com>
2019-12-04 00:46:12 +0200

    Removed comments

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,3 @@
 
 Route::get('/', 'HomeController@home')->name('home');
 Route::get('/blog/{slug}', 'HomeController@blogPost')->name('post');
-
-
-
-// tests
-/**
- * 1. Each blog post has multiple hardcoded slugs (write only)
- * 2. Every secondary slug permanent redirects to the primary slug
- * 3. Make sure there is not duplicate slug
- * 4. Each blog post page has the required meta tags
- *      title, description, og:image, etc.
- * 5. Each blog post page returns a 200 status
- */
