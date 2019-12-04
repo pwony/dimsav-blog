bda17559feea9257b720d5aff495dbcdc6a5f16b
pwony <p.wony0526@gmail.com>
2019-12-04 14:23:47 +0200

    Temp disable https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
+            // URL::forceScheme('https');
         }
     }
 }
