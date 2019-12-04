8486f43b7e767c8cd83cc8c6b061f46f5dad0914
pwony <p.wony0526@gmail.com>
2019-12-04 11:11:49 +0200

    Replace static data with dynamic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,20 +26,21 @@ class HomeController extends Controller
 
     public function blogPost($slug)
     {
-        if ($slug != 'how-to-use-vuejs-on-jquery-websites') {
-            return redirect(route('blog-post', 'how-to-use-vuejs-on-jquery-websites'));
-        }
+        $post = BlogPost::findBySlug($slug);
+//        if ($slug != 'how-to-use-vuejs-on-jquery-websites') {
+//            return redirect(route('blog-post', 'how-to-use-vuejs-on-jquery-websites'));
+//        }
 
         $head_description = 'One of the reasons it was very hard for me to start using vueJs for a '.
             'long time on production is because I was mostly coding on existing websites whose '.
             'frontend was built with jQuery.';
 
         return view('post', [
-            'head_canonical_url' => route('blog-post', $slug),
-            'head_page_title' => 'How to use VueJs on jQuery websites', // (max 70 characters)
+            'head_canonical_url' => $post->url,
+            'head_page_title' => $post->title, // (max 70 characters)
             'head_description' => $head_description, //  (maximum 200 characters)
             'head_og_type' => 'article',
-            'head_image' => '', // less than 5MB (duuhh)
+            'head_image' => $post->image_url, // less than 5MB (duuhh)
             'head_image_width' => '', // number in pixels
             'head_image_height' => '', // number in pixels
             'head_article_tags' => collect(['jQuery', 'vueJs']),
