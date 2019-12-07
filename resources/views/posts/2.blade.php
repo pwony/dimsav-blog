4ce92c586f3385bdd5f190bc22689c5b1463a8c5
pwony <p.wony0526@gmail.com>
2019-12-08 01:58:22 +0200

    removed paragraph styles duplication

--- resources/views/posts/2.blade.php
+++ resources/views/posts/2.blade.php
@@ -2,60 +2,60 @@
 
 @section('post-content')
 
-    <p class="text-xl md:text-2xl px-5 my-12">
+    @component('p')
         Some time ago I was having a chat with a high ranked executive of a popular cartoon tv
         channel targeted to kids. He was a nice person.
-    </p>
+    @endcomponent
 
-    <p class="text-xl md:text-2xl px-5 my-12">
+    @component('p')
         He was bragging about how he managed to save the company from bankruptcy, even though they
         had to show illegally "sometimes" (I felt he was implying "most of the times") commercials
         targeted to kids on their channel, exceeding the ad duration per how allowed by the law.
-    </p>
+    @endcomponent
 
-    <p class="text-xl md:text-2xl px-5 my-12">
+    @component('p')
         One day, the tv at home was on this channel, and my kids were watching these commercials
         promoting toys. They were so aggressive I felt disgusted. Later the same day, my son asked
         me (of course) to buy him many of the toys he watched on the ads earlier.
-    </p>
+    @endcomponent
 
-    <p class="text-xl md:text-2xl px-5 my-12">
+    @component('p')
         Don't get me wrong. I love buying toys to my children, as they are a necessary part of their
         life. My problem with ads is that they produce fake needs, without limit. They make us feel
         we can only be happy if we buy what they sell.
-    </p>
-    <p class="text-xl md:text-2xl px-5 my-12">
+    @endcomponent
+    @component('p')
         I felt obliged as a parent to prevent my kids being brainwashed by those aggressive ads
         since then. Of course children want to watch cartoons. So what are the alternatives?
-    </p>
+    @endcomponent
 
-    <p class="text-xl md:text-2xl px-5 my-12">
+    @component('p')
         The easiest and simplest solution for us was Netflix.
-    </p>
+    @endcomponent
 
-    <p class="text-xl md:text-2xl px-5 my-12">
+    @component('p')
         We prefer paying a monthly subscription than having our kids watch "free" tv but being
         brainwashed from ads. Sometimes I wonder, am I overprotective?
-    </p>
+    @endcomponent
 
-    <p class="text-xl md:text-2xl px-5 my-12">
+    @component('p')
         Am I doing more harm by isolating a kid from what nowadays is considered the norm? I believe
         and I hope I'm not the only one taking the same decision.
-    </p>
+    @endcomponent
 
-    <p class="text-xl md:text-2xl px-5 my-12">
+    @component('p')
         And if the number of parents choosing "non free" but ad-less tv increases over time,
         probably this is a good thing. Because studios that produce "good" content are promoted
         versus those who promote advertisement time for their customers.
-    </p>
+    @endcomponent
 
-    <p class="text-xl md:text-2xl px-5 my-12">
+    @component('p')
         I'm not a fan of Netflix, but I'm afraid that's the best alternative we have right now, and
         maybe this means that the market of subscription based tv will increase in the future. And I
         believe this is a good thing.
-    </p>
-    <p class="text-xl md:text-2xl px-5 my-12">
+    @endcomponent
+    @component('p')
         Time will show.
-    </p>
+    @endcomponent
 
 @endsection
