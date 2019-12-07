60ce032d4783e1f62bebe342156d830b1429fdb5
pwony <p.wony0526@gmail.com>
2019-12-07 23:55:55 +0200

    Renamed class

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\BlogPost;
 
-class HomeController extends Controller
+class BlogController extends Controller
 {
     public function home()
     {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-Route::get('/', 'HomeController@home')->name('home');
-Route::get('/blog/{slug}', 'HomeController@blogPost')->name('blog-post');
+Route::get('/', 'BlogController@home')->name('home');
+Route::get('/blog/{slug}', 'BlogController@blogPost')->name('blog-post');
 
 Route::get('/blog/posts/testing-a-live-site-with-laravel-dusk-using-docker-compose-20',
     'SeoController@duskArticle');
