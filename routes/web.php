2a4a1951aec175b9df8c02dfd4e856eedcb7fb3c
pwony <p.wony0526@gmail.com>
2019-12-08 17:28:38 +0200

    Added old article

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,11 @@ Route::get('/blog/9/git-repository-inside-composer-vendors',
     'SeoController@composerArticle');
 Route::get('/blog/14/free-email-with-your-own-domain',
     'SeoController@goHome');
+Route::get('contact', 'SeoController@goHome');
+Route::get('about', 'SeoController@goHome');
+Route::get('index.html', 'SeoController@goHome');
+Route::get('laravel-translatable', 'SeoController@goHome');
+Route::get('blog/12/phpstorm-phpspec', 'SeoController@goHome');
+Route::get('h/8365374.html', 'SeoController@goHome');
+Route::get('blog/11/cool-javascript-stuff-i-learned-at-the-open-techschool-workshop', 'SeoController@goHome');
+Route::get('blog/5/apache-redirecting-www-to-no-www', 'SeoController@goHome');
